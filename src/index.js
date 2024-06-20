0ac98db18abbace24eb7ab668728d93c810cf850
Psanjay-15 <sanjaypandere26@gmail.com>
2024-06-20 13:06:51 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -6,8 +6,16 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-
-connectDB();
+const PORT = process.env.PORT || 8000;
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongo connection fail", err);
+  });
 
 // const app = express()(async () => {
 //   try {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+// const asynHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
+
+const asynHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
